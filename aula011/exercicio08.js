7929b6436648853d93c1e0c6b08b5e973f3775de
Germano007 <germanomanoel007@gmail.com>
2023-03-13 09:16:55 -0300

    atualizado

---
+++ aula011/exercicio08.js
@@ -0,0 +1,8 @@
+var vel = 80.5;
+console.log(`A velocidade do seu carro é ${vel} KM/h!`);
+
+if(vel >= 60){
+    console.log(`Você ultrapassou a velocidade permitida que é de 60km/h! Multado!`)
+}else{
+    console.log(`Dirija sempre usando cinto de segurança.`)
+}
