e8b9fea39ee16c09c876ca657d94e5242c2ecf0e
Germano007 <germanomanoel007@gmail.com>
2023-03-27 09:28:44 -0300

    Atualizado com mais ex e desa

--- aula011/exercicio08.js
+++ aula011/exercicio08.js
@@ -1,8 +1,8 @@
-var vel = 80.5;
+var vel = 70;
 console.log(`A velocidade do seu carro é ${vel} KM/h!`);
 
-if(vel >= 60){
-    console.log(`Você ultrapassou a velocidade permitida que é de 60km/h! Multado!`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida que é de 60km/h, Multado!`)
 }else{
-    console.log(`Dirija sempre usando cinto de segurança.`)
+    console.log(`Dirija com atenção e sempre usando cinto de segurança.`)
 }
--- aula011/exercicio09.js
+++ aula011/exercicio09.js
@@ -1,6 +1,6 @@
-var pais = 'Brasil';
+var pais = 'brasil';
 console.log(`Vivendo em ${pais}`);
-if(pais == "Brasil"){
+if(pais == "Brasil","brasil","Brazil","brazil"){
     console.log('Você é Brasileiro!');
 } else{
     console.log('Você é Estrangeiro!');
